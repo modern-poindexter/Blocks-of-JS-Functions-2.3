dfff96a3499e931a6c82c8956f31428110731aba
Mikami Archer <notasentientrobot@gmail.com>
2026-03-29 15:43:46 -0400

    styling and function commit

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Assignment 2.3: The Building Blocks of Javascript (JS) - Functions
 
 Decided to make this just a simple Rock, Paper, Scissors gane. It is a bit of a combination of my prior knowledge and what is being learned in this unit.
+
+Ran into a few snafus in creation but seeing what works and still trying ti expand on my learning.
--- innerHTML.js
+++ innerHTML.js
@@ -1,5 +1,41 @@
+// Official game play functionality
+
 function updateText() {
     document.getElementById("the-head").innerHTML = "Time to Crush, Cover, and Cut!"
 }
 
 
+const options = ["crush", 'cover', 'cut'];
+const human = document.getElementById('human');
+const computer = document.getElementById('computer');
+const winner = document.getElementById('winner');
+const scores = document.getElementsByClassName('scores');
+let scores = 0;
+
+
+
+function playtime(gameChoices) {
+    const computerPlay = options[Math.floor(Math.random() * 3)];
+    let result = '';
+    if(humanPlay === computerPlay){
+        result = 'TIED UP!';
+    }
+    else {
+        switch(humanPlay) {
+            case 'crush';
+                result = (computerPlay === 'cut') ? 'WINNER!' : 'LOSER!';
+                break;
+            case 'cut';
+                result = (computerPlay === 'cover') ? 'WINNER!' : 'LOSER!';
+                break;
+            case 'cover';
+                result = (computerPlay === 'cut') ? 'WINNER!' : 'LOSER!';
+                break;
+        }
+    }
+
+    human.textContent = 'You: '$(humanPlay);
+    computer.textContent = 'AI: '$(computerPlay);
+    winner.textContent = 'And the winner is: '(result);
+
+}
